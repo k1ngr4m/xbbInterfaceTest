24dc9a98eab6bc51c4e3376fa1d4c062de408a0a
k1ngr4m <365717424@qq.com>
2023-03-29 13:13:43 +0800

    2023/03/29 update:jenkins参数触发更新数据库

--- testcase/test_run.py
+++ testcase/test_run.py
@@ -14,7 +14,6 @@ from utils.yapiutil import Yapi
 from config.jenkinsparam import environment
 from config.jenkinsparam import isupdate_database
 
-yapi = Yapi()
 case_data = RdTestcase()
 case_list_positive = case_data.is_run_data('xbb', 1)
 case_list_negative = case_data.is_run_data('xbb', 0)
@@ -23,10 +22,6 @@ current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 class TestApi:
     def setup_class(self):
-        if isupdate_database == "true":
-            logger.info(f"***** 开始执行更新数据库测试用例，开始时间为：{current_time} *****")
-            yapi.updateDatabase()
-            logger.info(f"***** 执行更新数据库测试用例完成，完成时间为：{current_time} *****")
         logger.info(f"***** 开始执行测试用例，开始时间为：{current_time} *****")
 
     def teardown_class(self):
